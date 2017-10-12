203b07849915f345da7e1d89a44eb9ecfd8b2e67
Avi Miller <TrebecS@Gmail.com>
2017-10-11 18:29:17 -0700

    Empty Commit
    Just setting up the repo!

---
+++ Assets/Welcome.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ * This is a test class to see if the repo works!
+ * 
+ */
+
+public class Welcome{
+
+}
